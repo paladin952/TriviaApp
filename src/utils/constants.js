e601ef47ba1a802aff5331401777c4f6799c032c
Clapa Lucian <lucian@dealdash.com>
2018-03-06 18:00:21 +0200

    add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+
+
+export const SERVER_API = 'https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean';
---
+++ src/utils/strings.js
@@ -0,0 +1,18 @@
+import I18n from 'react-native-i18n';
+
+
+I18n.translations = {
+    en: {
+        menu_title: 'Welcome to the Trivia Challenge',
+        menu_challenge: 'You will be presented with 10 True or Falsue questions',
+        menu_can_you_score: 'Can you score 100%?',
+        menu_begin_button: 'BEGIN',
+        generic_error: 'Something went wrong. Please try again!',
+        retry: 'Retry',
+        play_again: 'PLAY AGAIN?',
+        you_scored: 'You scored\n{{score}} / {{numberOfQuestions}}'
+    }
+};
+
+I18n.fallbacks = true;
+export default I18n;
